6c356ee0905c05bd7619b81eca9eb58663ac2bc5
erik dooper <3162919+edooper@users.noreply.github.com>
2026-07-24 13:21:43 +0200

    TEMP: wire-level event-server instrumentation for Info-event debugging
    Logs every inbound NOTIFY (method, SID, SEQ, Transfer-Encoding,
    Content-Length, body length, current observer SIDs) and flags any
    packet that matches no observer. Goes to stdout / docker logs. To be
    removed once the missing-Info-events cause is identified.

--- Upnp/EventServer.py
+++ Upnp/EventServer.py
@@ -183,6 +183,7 @@ class EventServer(Thread):
             if listenSock in iret:
                 # A device is requesting a connection
                 (sock, addr) = listenSock.accept()
+                print('[EvtDbg] new connection from %s' % (addr,), flush=True)
                 connSocks.append(sock)
                 connSessions[sock] = EventSession(sock, addr)
 
@@ -231,18 +232,31 @@ class EventServer(Thread):
                                 # Find the observer to notify
                                 sid = recvpkt.Header('SID')
                                 seq = recvpkt.Header('SEQ')
+                                method = recvpkt.Request()[0]
+                                print('[EvtDbg] pkt method=%s sid=%s seq=%s TE=%s CL=%s bodylen=%d observers=%r'
+                                      % (method, sid, seq,
+                                         recvpkt.Header('Transfer-Encoding'),
+                                         recvpkt.Header('Content-Length'),
+                                         len(recvpkt.Body()),
+                                         [o.SubId() for o in self.iObservers]),
+                                      flush=True)
                                 if sid==None or seq==None:
                                     # Invalid packet
                                     pass
                                 else:
+                                    matched = False
                                     for obs in self.iObservers:
                                         if sid == obs.SubId():
+                                            matched = True
                                             # A misbehaving observer must not kill
                                             # the server thread
                                             try:
                                                 obs.Notify( seq, recvpkt.Body() )
                                             except Exception:
                                                 traceback.print_exc()
+                                    if not matched:
+                                        print('[EvtDbg] NO observer matched sid=%s body[:200]=%r'
+                                              % (sid, recvpkt.Body()[:200]), flush=True)
 
                                 # Send a response to acknowledge
                                 ack = HttpPacket.HttpResponse()
